79ee7a90bb5c5ecdec1f59c51323114bbdbb1fff
Tara Kaul <Tara.Kaul@softwire.com>
2023-07-07 11:20:59 +0100

    fixed errors in SQL

--- initialSQL.sql
+++ initialSQL.sql
@@ -5,6 +5,13 @@ CREATE TABLE Book (
     PRIMARY KEY(ISBN)
 );
 
+
+CREATE TABLE Author (
+     AuthorID varchar(255),
+     AuthorName varchar(255),
+     PRIMARY KEY(AuthorID)
+);
+
 CREATE TABLE Wrote (
     AuthorID varchar(255),
     ISBN varchar(255),
@@ -13,10 +20,10 @@ CREATE TABLE Wrote (
     FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID)
 );
 
-CREATE TABLE Author (
-     AuthorID varchar(255),
-     AuthorName varchar(255),
-     PRIMARY KEY(AuthorID)
+CREATE TABLE Users (
+    UserID int,
+    Username varchar(255),
+    PRIMARY KEY (UserID)
 );
 
 CREATE TABLE Borrowing (
@@ -27,9 +34,4 @@ CREATE TABLE Borrowing (
     PRIMARY KEY (ID),
     FOREIGN KEY (ISBN) REFERENCES Book(ISBN),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
-
-CREATE TABLE Users (
-    UserID int,
-    Username varchar(255),
-    PRIMARY KEY (UserID)
 );
