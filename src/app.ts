3182a9fbd64e0755ff3358d1c61e5a1bc8042c28
Tara Kaul <Tara.Kaul@softwire.com>
2023-07-07 14:13:18 +0100

    changed message into an array in /books

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,63 @@ app.listen(port, () => {
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
 
+//INITAL CONNECTION
+var Request = require('tedious').Request;
+var Connection = require('tedious').Connection;
 
+//configuration for login
+var config = {
+  server: "localhost", 
+  options: {
+    database: 'bookish',
+    port: 1433,
+    trustServerCertificate: true
+  },
+  authentication: {
+    type: "default",
+    options: {  
+      userName: "bookishUser",
+      password: "bookishPassword!14",
+    }
+  }
+};
 
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+    if(err) {
+      console.log('Error: ', err)
+    }
+    // If no error, then good to go...
+    
+  });
+connection.connect();
 
+//BOOKS ENDPOINT TO RETURN ALL BOOKS
 app.get('/books', (req, res) => {
-    res.send('Hello World');
+    let message = []
+
+    let request = new Request("SELECT * FROM Book", function(err, rowCount) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(rowCount + ' rows');
+          // and we close the connection
+          connection.close()
+        }
+      });
+
+    request.on('row', function(columns) {
+        let tempArray = []
+        columns.forEach(function(column) {
+        //   console.log(column.value);
+          tempArray.push(column.value)
+        });
+        message.push(tempArray)
+        tempArray = []
+        console.log(message)
+    });
+
+
+    connection.execSql(request);
+    res.send(message);
 });
