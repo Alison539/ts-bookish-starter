f4bbd45be2ddece06ac90bc02b97011f59dd6393
Tara Kaul <Tara.Kaul@softwire.com>
2023-07-07 11:15:52 +0100

    Added SQL for initial database setup

---
+++ initialSQL.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Book (
+    Title varchar(255),
+    ISBN varchar(255),
+    numberOfCopies int,
+    PRIMARY KEY(ISBN)
+);
+
+CREATE TABLE Wrote (
+    AuthorID varchar(255),
+    ISBN varchar(255),
+    PRIMARY KEY (AuthorID, ISBN),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN),
+    FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID)
+);
+
+CREATE TABLE Author (
+     AuthorID varchar(255),
+     AuthorName varchar(255),
+     PRIMARY KEY(AuthorID)
+);
+
+CREATE TABLE Borrowing (
+    ID int,
+    ISBN varchar(255), 
+    UserID int,
+    DueDate date,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+
+CREATE TABLE Users (
+    UserID int,
+    Username varchar(255),
+    PRIMARY KEY (UserID)
+);
