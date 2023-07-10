70e9b974803da31ae53b7c56a4b5956f3031e169
Tara Kaul <Tara.Kaul@softwire.com>
2023-07-10 16:33:27 +0100

    fixed connection hanging errors

--- src/app.ts
+++ src/app.ts
@@ -57,15 +57,15 @@ var config = {
   }
 };
 
-var connection = new Connection(config);
-connection.on('connect', function(err) {
-    if(err) {
-      console.log('Error: ', err)
-    }
-    // If no error, then good to go...
+// var connection = new Connection(config);
+// connection.on('connect', function(err) {
+//     if(err) {
+//       console.log('Error: ', err)
+//     }
+//     // If no error, then good to go...
     
-  });
-connection.connect();
+//   });
+
 
 app.get('/login', (req, res) => {
   //let token = jwt.sign({ foo: 'bar' }, 'shhhhh');
@@ -73,100 +73,105 @@ app.get('/login', (req, res) => {
 })
 
 function checkCredentials(username,password,res){
-  let checkAuthenticate = new Request("SELECT Passwords FROM Logins WHERE Usernames = '" + username + "' AND Passwords = '" + password + "'",  function(err, rowCount,rows) {
-    if (err) {
-      console.log(err)
-      return res.status(400).json({
-        message: 'Something is not right',
-        user   : false
-    });
-    } else {
-      connection.close()
-      if(rowCount === 0){
-        return res.status(400).json({
-          message: "Incorrect username/password",
-          user   : false
+  var connection3 = new Connection(config);
+  connection3.on('connect', function(err) {
+    if(err) {
+      console.log('Error: ', err)
+    }
+    else {
+      let checkAuthenticate = new Request("SELECT Passwords FROM Logins WHERE Usernames = '" + username + "' AND Passwords = '" + password + "'",  function(err, rowCount,rows) {
+        if (err) {
+          console.log(err)
+          return res.status(400).json({
+            message: 'Something is not right',
+            user   : false
         });
-      }
-      else{
-        const token = jwt.sign({username: username}, 'super secret');
-        return res.json({username, token});
-        res.send("Correct username and password")
-      }
-      // and we close the connection
-      
+        } else {
+          connection3.close()
+          if(rowCount === 0){
+            return res.status(400).json({
+              message: "Incorrect username/password",
+              user   : false
+            });
+          }
+          else{
+            const token = jwt.sign({username: username}, 'super secret');
+            return res.json({username, token});
+            res.send("Correct username and password")
+          }
+          // and we close the connection
+          
+        }
+      });
+      connection3.execSql(checkAuthenticate)
     }
   });
-  connection.execSql(checkAuthenticate)
-}
-export function confirmCredentials(username,password){
-  let checkAuthenticate = new Request("SELECT Passwords FROM Logins WHERE Usernames = " + username + "AND Passwords = " + password,  function(err, rowCount,rows) {
-    if (err) {
-      console.log( err)
-      return false;
-    } else {
-      // and we close the connection
-      connection.close()
-      if(rowCount === 0){
-       return false;
-      }
-      else{
-        return true;
-      }
-      return false;
-      
-    }
-  });
-  connection.execSql(checkAuthenticate)
-  //return true;
+
+  connection3.connect()
+
+  connection3.close()
 }
 
 
 //BOOKS ENDPOINT TO RETURN ALL BOOKS
 app.get('/books', passport.authenticate('jwt', {session: false}), 
   function (req, res) {
-
-  var books = new AllBooks();
+  var connection2 = new Connection(config);
+  connection2.on('connect', function(err) {
+      if(err) {
+        console.log('Error: ', err)
+      }
+      // If no error, then good to go...
+      else {
+        var books = new AllBooks();
   
   
-  let getGeneral = new Request("SELECT ISBN, Title, numberOfCopies FROM Book \n SELECT ISBN, AuthorName FROM Wrote JOIN Author ON Wrote.AuthorID = Author.AuthorID \n SELECT ISBN, DueDate, Username, Borrowing.UserID FROM Borrowing JOIN Users ON Borrowing.UserID = Users.UserID", function(err, rowCount) {
-    if (err) {
-      console.log(err);
-    } else {
-      // and we close the connection
-      connection.close()
-      //returning information about the books
-      res.send(JSON.stringify(books));
-    }
-  });
+        let getGeneral = new Request("SELECT ISBN, Title, numberOfCopies FROM Book \n SELECT ISBN, AuthorName FROM Wrote JOIN Author ON Wrote.AuthorID = Author.AuthorID \n SELECT ISBN, DueDate, Username, Borrowing.UserID FROM Borrowing JOIN Users ON Borrowing.UserID = Users.UserID", function(err, rowCount) {
+          if (err) {
+            console.log(err);
+          } else {
+            // and we close the connection
+            connection2.close()
+            //returning information about the books
+            res.send(JSON.stringify(books));
+          }
+        });
+      
+        //this gets called just before connection.close() above
+        getGeneral.on('row', function(columns) {
+      
+          //temp array stores the contents of a row (a record from the returned query)
+          let tempArray = []
+          columns.forEach(function(column) {
+            tempArray.push(column.value)
+          });
+      
+          let numberItems = tempArray.length
+          if (numberItems == 3){
+              //it is the general (ISBN Title numberofCopies)
+              books.newBook(tempArray)
+          }
+          else if (numberItems == 2){
+              //it is an author
+              books.setAuthors(tempArray)
+          }
+          else if (numberItems == 4){
+            //its about an unavailable book
+              books.setUnavailable(tempArray)
+      
+          }   
+        
+      });
+      connection2.execSql(getGeneral);
+      }
+      
+      
+      });
+  connection2.connect()
 
-  //this gets called just before connection.close() above
-  getGeneral.on('row', function(columns) {
 
-    //temp array stores the contents of a row (a record from the returned query)
-    let tempArray = []
-    columns.forEach(function(column) {
-      tempArray.push(column.value)
-    });
 
-    let numberItems = tempArray.length
-    if (numberItems == 3){
-        //it is the general (ISBN Title numberofCopies)
-        books.newBook(tempArray)
-    }
-    else if (numberItems == 2){
-        //it is an author
-        books.setAuthors(tempArray)
-    }
-    else if (numberItems == 4){
-      //its about an unavailable book
-        books.setUnavailable(tempArray)
-
-    }   
-  
-});
-
-connection.execSql(getGeneral);
+  connection2.close()
 
 
     
